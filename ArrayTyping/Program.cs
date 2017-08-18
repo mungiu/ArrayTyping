65d8af97a2b76ac590c41bf9aab4023d20554ef0
Andrei Mungiu <andrei_mungiu@yahoo.com>
2017-08-18 12:07:18 +0200

    Tutorial checkpoint

--- ArrayTyping/Program.cs
+++ ArrayTyping/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace ArrayTyping
 {
@@ -10,6 +11,26 @@ namespace ArrayTyping
     {
         static void Main(string[] args)
         {
+            //this array can hold anything 
+            //because all types are implicitly derived from "object"
+            object[] objArray = new object[3]
+            {
+                "Hello world",
+                4,
+                new Button { Text="Click me!" }
+            };
+
+            //using reflection to get type
+            Type objArrType = objArray.GetType();
+
+            string[] strArray = new string[2];
+            Type strArrType = strArray.GetType();
+
+            //displaying the content of each object in the array
+            foreach (object item in objArray)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
